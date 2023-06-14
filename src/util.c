9897b7eb4bda2975d6f88b376ba2d8cbc628ca64
james-parky <72101291+james-parky@users.noreply.github.com>
2023-06-14 10:55:47 +0100

    :memo: Documentation
    Added a file header and documentation to each function.

--- src/util.c
+++ src/util.c
@@ -1,9 +1,31 @@
+/******************************************************************************
+* FILENAME :        util.c                                                    *
+*                                                                             *
+* DESCRIPTION :                                                               *
+*       Functions to start and stop the required terminal mode for drawing    *
+*       windows using twin.                                                   *
+*                                                                             *
+* NOTES :                                                                     *
+*       See https://github.com/james-parky/twin for more detail.              *
+*                                                                             *
+*       Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>  *
+*       Everyone is permitted to copy and distribute verbatim copies of this  *
+*       licensed document, but changing it is not allowed.                    *
+*                                                                             *
+* AUTHOR :    James David Parkington        START DATE :    04 May 23         *
+******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "util.h"
+#include "twin/util.h"
 
-void terminal_start_config () {
+/******************************************************************************
+* twin_terminal_start_config:                                                 *
+*       Prepare the terminal for drawing windows with twin by entering it     *
+*       into the alternate mode, making the cursor invisible, positioning the *
+*       cursor at [0,0] and setting stty type to raw.                         *   
+******************************************************************************/
+void twin_terminal_start_config () {
     system("tput smcup");
     printf("\e[?25l");
     printf("\e[?30l");
@@ -11,15 +33,26 @@ void terminal_start_config () {
     system("/bin/stty raw");
 }
 
-void terminal_stop_config () {
+/******************************************************************************
+* twin_terminal_stop_config:                                                  *
+*       Reset the terminal back to regular by making the cursor visible       *
+*       again, setting stty back to cooked, and exiting alternate mode.       *
+******************************************************************************/
+void twin_terminal_stop_config () {
     system("/bin/stty cooked");
     printf("\e[?25h");
     system("tput rmcup");
 
 }
-void stop (int exit_code) {
+
+/******************************************************************************
+* twin_terminal_stop_config:                                                  *
+*       Reset the terminal back to regular by making the cursor visible       *
+*       again, setting stty back to cooked, and exiting alternate mode.       *
+******************************************************************************/
+void twin_stop (int exit_code) {
     system("clear");
-    terminal_stop_config();
+    twin_terminal_stop_config();
     exit(exit_code);
 }
 
